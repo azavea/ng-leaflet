d2cc89691ee40d06f9515bb18c728dadb8445a6f
CloudNiner <andrew@cloudniner.co>
2015-12-23 10:52:13 -0500

    Add helper AZLeafletComponentFactory

--- dist/azavea-ng-leaflet.js
+++ dist/azavea-ng-leaflet.js
@@ -11,6 +11,41 @@
 (function () {
     'use strict';
 
+    function LeafletComponent() {
+        return LeafletComponentFactory;
+        /**
+         * Return a new LeafletComponent directive definition object
+         * for use in a directive definition
+         *
+         * Override component.onMapReady(map) to configure the map
+         *
+         * See examples/002-basic-directive.html for an example
+         */
+        function LeafletComponentFactory(directive) {
+            var component = angular.extend({}, {
+                restrict: 'E',
+                require: '^azLeaflet',
+                scope: true,
+                onMapReady: angular.noop
+            }, directive);
+            // Assign after creation so that the proper component.onMapReady
+            // exists in link function below
+            component.link = link;
+            return angular.extend({}, component, directive);
+
+            function link(scope, element, attrs, controller) {
+                controller.getMap().then(component.onMapReady);
+            }
+        }
+    }
+
+    angular.module('az.leaflet')
+        .factory('AZLeafletComponentFactory', LeafletComponent);
+})();
+
+(function () {
+    'use strict';
+
     /** Provides access to the leaflet map object instantiated by the directive */
     /*@ngInject*/
     LeafletController.$inject = ["$timeout", "$q"];
